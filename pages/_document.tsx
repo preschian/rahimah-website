e7f9e4caf8920ccf089542104aebd680c816b5ec
Preschian Febryantara <preschian@users.noreply.github.com>
2022-12-11 15:52:15 +0700

    feat: upgrade to nextjs (#9)

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700|Raleway&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
