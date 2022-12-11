0c0299af1719b08e205e1f17cefedee15b1b143e
Preschian Febryantara <preschian27@gmail.com>
2022-12-11 15:14:34 +0700

    feat: add post detail

---
+++ pages/[slug].jsx
@@ -0,0 +1,39 @@
+import Header from "../components/Header"
+
+export async function getServerSideProps({ params }) {
+  const url = process.env.API_ENDPOINT || 'http://localhost:3000/'
+  const res = await fetch(`${url}/posts/slug:${params.slug}`)
+  const data = await res.json()
+
+  return {
+    props: {
+      featuredMedia: data.post_thumbnail && data.post_thumbnail.URL + "?w=900",
+      title: data.title,
+      content: data.content
+    }
+  }
+}
+
+export default function Post({ featuredMedia, title, content }) {
+  return (
+    <div>
+      <div className="bg-white">
+        <Header />
+      </div>
+
+      <div className="container py-16" style={{ maxWidth: '900px' }}>
+        <div className="bg-white">
+          <picture>
+            <img src={featuredMedia} alt={title} className='block w-full' />
+          </picture>
+
+          <div className="container p-8" style={{ maxWidth: '600px' }}>
+            <p className="text-4xl font-bold">{title}</p>
+
+            <div className="post" dangerouslySetInnerHTML={{ __html: content }}></div>
+          </div>
+        </div>
+      </div>
+    </div >
+  )
+}
